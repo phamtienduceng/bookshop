d59b897e2e6fbd5917e4b94dad7fa593a8d4e77e
hoangtrung9861 <115148880+hoangtrung9861@users.noreply.github.com>
2023-06-16 09:51:46 +0700

    Trung

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,10 +10,16 @@ class Category extends Model
 
 
 
-    
+
     use HasFactory;
     protected $fillable=['name', 'slug'];
 
+
+
+
+
+
+    
     public function products()
     {
         return $this->hasMany(Product::class, 'category_id'); // join table
