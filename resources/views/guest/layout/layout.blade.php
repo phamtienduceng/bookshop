8a37a1e7af3041cfe4d98932e7ecde7d35f4811f
leuleu12321321312321312312 <phuhoang136@gmail.com>
2023-06-16 09:41:13 +0700

    phu

--- resources/views/guest/layout/layout.blade.php
+++ resources/views/guest/layout/layout.blade.php
@@ -57,12 +57,21 @@
 				          <li><a href="{{ Route('articles')}}">Articles</a></li>
 						  <li><a href="{{ Route('aboutUs')}}">About us</a></li>
 				          <li><a href="{{ Route('contactUs')}}">Contact us</a></li>
+				          @guest
 				          <li class="menu-has-children"><a href="">User</a>
 				            <ul>
-				              <li><a href="{{ Route('login') }}">Login</a></li>
-				              <li><a href="{{ Route('register') }}">Sign up</a></li>
+				              <li><a class="{{ (request()->is('login')) ? 'active' : '' }}" href="{{ Route('login') }}">Login</a></li>
+				              <li><a class="{{ (request()->is('register')) ? 'active' : '' }}" href="{{ Route('register') }}">Sign up</a></li>
+                            </ul>
+                          </li> 
+							  @else 
+						  <li class="menu-has-children"><a href="">{{auth()->user()->firstname}} {{auth()->user()->lastname}}</a>
+							<ul> 
+							  <li><a class="" href="{{ Route('profile') }}">Profile</a></li>
+				              <li><a class="" href="{{ Route('logout') }}">LogOut</a></li> 
 				            </ul>
 				          </li>
+						  @endguest
 				        </ul>
 				      </nav><!-- #nav-menu-container -->		    		
 			    	</div>
