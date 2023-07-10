236713731f9b4d93a21c48b24f1b80634def0407
phamtienduceng <phamtienduceng@gmail.com>
2023-07-10 22:54:59 +0700

    duc commit

--- resources/views/guest/layout/layout.blade.php
+++ resources/views/guest/layout/layout.blade.php
@@ -29,6 +29,8 @@
     <link rel="stylesheet" href="{{ asset('guest/css/animate.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('guest/css/owl.carousel.css') }}" />
     <link rel="stylesheet" href="{{ asset('guest/css/main.css') }}" />
+    <link rel="stylesheet" href="{{ asset('guest/css/layout.css') }}">
+
 
     <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/styles/bootstrap4/bootstrap.min.css') }}" />
     <link href="{{ asset('guest/product/plugins/font-awesome-4.7.0/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
@@ -78,6 +80,7 @@
 					</ul>
 					</nav><!-- #nav-menu-container -->		    		
 				</div>
+
 			</div>
 	</header><!-- #header -->
 
@@ -85,60 +88,62 @@
 
 
     <!-- start footer Area -->
-    <footer class="footer-area section-gap">
+    <footer class="footer-area section-gap" style="padding-top: 50px;">
         <div class="container">
             <div class="row">
-                <div class="col-lg-5 col-md-6 col-sm-6">
+                <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="single-footer-widget">
-                        <h6>About Us</h6>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.
-                        </p>
-                        <p class="footer-text">
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        </p>
+                    <h6 class="header-footer">About us</h6>
+                        <ul class="ul-footer">
+                            <li>Location: District 3, Nam Ky Khoi Nghia street, Ho Chi Minh city</li>
+                            <li>Number: 012345678</li>
+                            <li>Contact: <a href="#" style="text-decoration: none">triducstore@gmail.com</a></li>
+                        </ul>
                     </div>
                 </div>
-                <div class="col-lg-5  col-md-6 col-sm-6">
+                <div class="col-lg-3  col-md-6 col-sm-6">
                     <div class="single-footer-widget">
-                        <h6>Newsletter</h6>
-                        <p>Stay update with our latest</p>
-                        <div class="" id="mc_embed_signup">
-                            <form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
-                                <input class="form-control" name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '" required="" type="email" />
-                                <button class="click-btn btn btn-default">
-                                    <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-                                </button>
-                                <div style="position: absolute; left: -5000px;">
-                                    <input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text" />
-                                </div>
+                        <h6 class="header-footer">Shopping</h6>
+                        <ul class="ul-footer">
+                            <li><a href="{{ Route('products') }}">Products</a></li>
+                            <li><a href="{{ Route('articles') }}">Articles</a></li>
 
-                                <div class="info"></div>
-                            </form>
-                        </div>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3  col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h6 class="header-footer">Information</h6>
+                        <ul class="ul-footer">
+                            <li><a href="{{ Route('contactUs') }}">About us</a></li>
+                            <li><a href="{{ Route('aboutUs') }}">Contact us</a></li>
+                        </ul>
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-6 col-sm-6 social-widget">
                     <div class="single-footer-widget">
-                        <h6>Follow Us</h6>
-                        <p>Let us be social</p>
-                        <div class="footer-social d-flex align-items-center">
-                            <a href="#">
+                    <img src="{{ asset('/images/logo.jpg')}}" alt="" class="logo-footer">
+                        <div class="footer-social d-flex align-items-center social">
+                            <a href="https://facebook.com">
                                 <i class="fa fa-facebook"></i>
                             </a>
-                            <a href="#">
+                            <a href="https://twitter.com">
                                 <i class="fa fa-twitter"></i>
                             </a>
-                            <a href="#">
-                                <i class="fa fa-dribbble"></i>
+                            <a href="https://google.com">
+                                <i class="fa fa-google"></i>
                             </a>
-                            <a href="#">
-                                <i class="fa fa-behance"></i>
+                            <a href="https://instagram.com">
+                                <i class="fa fa-instagram"></i>
                             </a>
                         </div>
                     </div>
+                </div>
+            </div>
+            <hr class="hr-footer">
+            <div class="row footer-end">
+                <div class="col-lg-12">
+                    Copyright 2023 Duc Tri Co. Ltd. All Right Reserve.
                 </div>
             </div>
         </div>
