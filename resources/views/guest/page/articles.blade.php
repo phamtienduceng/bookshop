4002e6c1fee3d6d6b640d198ceb5104934a3bff6
Tài tục tĩu <90624271+Aditerx@users.noreply.github.com>
2023-07-16 05:22:25 +0700

    c

--- resources/views/guest/page/articles.blade.php
+++ resources/views/guest/page/articles.blade.php
@@ -172,7 +172,7 @@
 								</div><!-- End /  -->
 
 								<!-- pagination -->
-								<ul class="pagination ml-50"><a class="pagination__item" href="#">1</a><a class="pagination__item" href="#">2</a><a
+								<ul class="pagination ml-75"><a class="pagination__item" href="#">1</a><a class="pagination__item" href="#">2</a><a
                                     class="pagination__item" href="#">3</a><span class="pagination__item active">4</span></ul><!-- End / pagination -->
 
 							</div>
