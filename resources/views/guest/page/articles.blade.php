01c075faf7d591061420a4f66d0b24100c92a3ff
Tài tục tĩu <90624271+Aditerx@users.noreply.github.com>
2023-07-16 05:26:09 +0700

    c

--- resources/views/guest/page/articles.blade.php
+++ resources/views/guest/page/articles.blade.php
@@ -42,8 +42,8 @@
         <!-- <link rel="stylesheet" href="{{ asset('guest/cart.css') }}"> -->
         <link rel="stylesheet" href="{{ asset('guest/css/article.css') }}">
 
-        {{-- <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/styles/bootstrap4/bootstrap.min.css') }}"> --}}
-        <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/styles/bootstrap4/bootstrap-grid.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/styles/bootstrap4/bootstrap.min.css') }}">
+        {{-- <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/styles/bootstrap4/bootstrap-grid.min.css')}}"> --}}
         <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/plugins/OwlCarousel2-2.2.1/owl.carousel.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/plugins/OwlCarousel2-2.2.1/animate.css') }}">
