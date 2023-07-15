2b124626020a5006665a9a8785b7c410841df3fd
Tài tục tĩu <90624271+Aditerx@users.noreply.github.com>
2023-07-16 02:06:36 +0700

    b

--- resources/views/guest/page/articles.blade.php
+++ resources/views/guest/page/articles.blade.php
@@ -1,6 +1,62 @@
-@extends('guest.layout.layout')
+<!DOCTYPE html>
+<html>
+	<head>
+        <!-- Mobile Specific Meta -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        {{-- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"> --}}
+		<meta name="format-detection" content="telephone=no">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+        <!-- Favicon-->
+        <link rel="shortcut icon" href="{{ asset('/images/onlyLogo.png') }}">
+        <!-- Author Meta -->
+        <meta name="author" content="codepixer">
+        <!-- Meta Description -->
+        <meta name="description" content="">
+        <!-- Meta Keyword -->
+        <meta name="keywords" content="">
+        <!-- meta character set -->
+        <meta charset="UTF-8">
+        <!-- Site Title -->
+        <title>Articles</title>
 
+        <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i&amp;amp;subset=latin-ext">
+        <!--
+        CSS
+        ============================================= -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <link rel="stylesheet" href="{{ asset('guest/css/linearicons.css') }}">
+        <link rel="stylesheet" href="{{ asset('guest/css/font-awesome.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('guest/css/bootstrap.css') }}">
+        <link rel="stylesheet" href="{{ asset('guest/css/magnific-popup.css') }}">
+        <link rel="stylesheet" href="{{ asset('guest/css/nice-select.css') }}">
+        <link rel="stylesheet" href="{{ asset('guest/css/animate.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('guest/css/owl.carousel.css') }}">
+        {{-- <link rel="stylesheet" href="{{ asset('guest/css/main.css') }}"> --}}
+        <link rel="stylesheet" href="{{ asset('guest/css/contact.css') }}">
+        <link rel="stylesheet" href="{{ asset('guest/css/layout.css') }}">
+        <!-- <link rel="stylesheet" href="{{ asset('guest/cart.css') }}"> -->
+        <link rel="stylesheet" href="{{ asset('guest/css/article.css') }}">
 
+        <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/styles/bootstrap4/bootstrap.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/styles/bootstrap4/bootstrap-grid.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/plugins/OwlCarousel2-2.2.1/owl.carousel.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/plugins/OwlCarousel2-2.2.1/animate.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/styles/main_styles.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/styles/responsive.css') }}">
+
+        <link rel="stylesheet" href="{{ asset('guest/product/plugins/themify-icons/themify-icons.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/plugins/jquery-ui-1.12.1.custom/jquery-ui.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/styles/single_styles.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('guest/product/styles/single_responsive.css') }}">
+
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
 		<div class="page-wrap">
 			<!-- Content-->
 			<div class="md-content">
@@ -238,4 +294,25 @@
 			</div>
 			<!-- End / Content-->
 
+
+
 		</div>
+		<!-- Vendors-->
+		<script type="text/javascript" src="assets/vendors/jquery/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/imagesloaded/imagesloaded.pkgd.js"></script>
+		<script type="text/javascript" src="assets/vendors/isotope-layout/isotope.pkgd.js"></script>
+		<script type="text/javascript" src="assets/vendors/jquery.countdown/jquery.countdown.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/jquery.countTo/jquery.countTo.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/jquery.countUp/jquery.countup.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/jquery.matchHeight/jquery.matchHeight.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/jquery.mb.ytplayer/jquery.mb.YTPlayer.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/masonry-layout/masonry.pkgd.js"></script>
+		<script type="text/javascript" src="assets/vendors/owl.carousel/owl.carousel.js"></script>
+		<script type="text/javascript" src="assets/vendors/jquery.waypoints/jquery.waypoints.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/menu/menu.min.js"></script>
+		<script type="text/javascript" src="assets/vendors/smoothscroll/SmoothScroll.min.js"></script>
+		<!-- App-->
+		<script type="text/javascript" src="assets/js/main.js"></script>
+	</body>
+</html>
