2db630cb1bb90a9c6dc238226742313c5ace780b
Tài tục tĩu <90624271+Aditerx@users.noreply.github.com>
2023-07-16 04:26:41 +0700

    c

--- resources/views/guest/page/articles.blade.php
+++ resources/views/guest/page/articles.blade.php
@@ -2,8 +2,6 @@
 
 @section('contents')
 
-<!DOCTYPE html>
-<html>
 	<head>
         <!-- Mobile Specific Meta -->
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -61,42 +59,7 @@
 	</head>
 
 	<body>
-        <header id="header" id="home" style="background-color: black; opacity: 0.9">
-            <div class="container">
-                <div class="row align-items-center justify-content-between d-flex">
-                    <div id="logo">
-                        <a href="{{ Route('home') }}"><img src="{{ asset('/images/logo.jpg') }}" style="width: 80px"></a>
-                    </div>
-                    <nav id="nav-menu-container">
-                        <ul class="nav-menu">
-                            <li class="menu-active"><a href="{{ Route('home') }}">Home</a></li>
-                            <li><a href="{{ Route('products') }}">Product</a></li>
-                            <li><a href="{{ Route('articles') }}">Articles</a></li>
-                            <li><a href="{{ Route('aboutUs') }}">About us</a></li>
-                            <li><a href="{{ Route('contactUs') }}">Contact us</a></li>
-                            <li><a href="{{ Route('cart.index') }}"><i class="fa-solid fa-cart-shopping"></i></a></li>
-                            <li><a href="{{ Route('wishlist.index') }}"><i class="fa-solid fa-heart"></i></a></li>
-                            @guest
-                            <li class="menu-has-children"><a href=""><i class="fa-solid fa-user"></i></a>
-                            <ul>
-                                <li><a class="{{ (request()->is('login')) ? 'active' : '' }}" href="{{ Route('login') }}">Login</a></li>
-                                <li><a class="{{ (request()->is('register')) ? 'active' : '' }}" href="{{ Route('register') }}">Sign up</a></li>
-                            </ul>
-                            </li>
-                                @else
-                            <li class="menu-has-children"><a href="">{{auth()->user()->name}}</a>
-                            <ul>
-                                <li><a class="" href="{{ Route('profile') }}">Profile</a></li>
-                                <li><a class="" href="{{ Route('password') }}">Change Password</a></li>
-                                <li><a class="" href="{{ Route('logout') }}">LogOut</a></li>
-                            </ul>
-                            </li>
-                            @endguest
-                        </ul>
-                    </nav><!-- #nav-menu-container -->
-                </div>
-            </div>
-        </header><!-- #header -->
+
 
 		<div class="page-wrap">
 			<!-- Content-->
@@ -339,70 +302,9 @@
 
 
 
-        <!-- start footer Area -->
-        <footer class="footer-area section-gap" style="padding-top: 50px;">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 col-sm-6">
-                        <div class="single-footer-widget">
-                        <h6 class="header-footer">About us</h6>
-                            <ul class="ul-footer">
-                                <li>Location: District 3, Nam Ky Khoi Nghia street, Ho Chi Minh city</li>
-                                <li>Number: 012345678</li>
-                                <li>Contact: <a href="#" style="text-decoration: none">triducstore@gmail.com</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3  col-md-6 col-sm-6">
-                        <div class="single-footer-widget">
-                            <h6 class="header-footer">Shopping</h6>
-                            <ul class="ul-footer">
-                                <li><a href="{{ Route('products') }}">Products</a></li>
-                                <li><a href="{{ Route('articles') }}">Articles</a></li>
 
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3  col-md-6 col-sm-6">
-                        <div class="single-footer-widget">
-                            <h6 class="header-footer">Information</h6>
-                            <ul class="ul-footer">
-                                <li><a href="{{ Route('contactUs') }}">About us</a></li>
-                                <li><a href="{{ Route('aboutUs') }}">Contact us</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-6 col-sm-6 social-widget">
-                        <div class="single-footer-widget">
-                        <img src="{{ asset('/images/logo.jpg')}}" alt="" class="logo-footer">
-                            <div class="footer-social d-flex align-items-center social">
-                                <a href="https://facebook.com">
-                                    <i class="fa fa-facebook"></i>
-                                </a>
-                                <a href="https://twitter.com">
-                                    <i class="fa fa-twitter"></i>
-                                </a>
-                                <a href="https://google.com">
-                                    <i class="fa fa-google"></i>
-                                </a>
-                                <a href="https://instagram.com">
-                                    <i class="fa fa-instagram"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <hr class="hr-footer">
-                <div class="row footer-end">
-                    <div class="col-lg-12">
-                        Copyright 2023 Duc Tri Co. Ltd. All Right Reserved.
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!-- End footer Area -->
 	</body>
-</html>
+
 
 		<!-- Vendors-->
 		<script src="{{ asset('guest/js/vendors/jquery/jquery.min.js')}}"></script>
